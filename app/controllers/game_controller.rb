9121dde98ed0bd8f1b0624125a82967512b637d7
Faraz Hussain <faraziit@gmail.com>
2016-10-25 09:35:21 -0500

    All done

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,4 +8,20 @@ class GameController < ApplicationController
     render("game/play_rock.html.erb")
   end
 
+  def user_plays_paper
+    moves =["rock", "paper", "scissors"]
+
+    @computer_move = moves.sample
+
+    render("game/play_paper.html.erb")
+  end
+
+  def user_plays_scissors
+    moves =["rock", "paper", "scissors"]
+
+    @computer_move = moves.sample
+
+    render("game/play_scissors.html.erb")
+  end
+
 end
